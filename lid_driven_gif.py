0227e446caaeafb325343f55a236e876b6c0377b
sseifi <sseifi@ip-10-23-131-2.uswe2.aws.amgen.com>
2025-05-30 21:55:56 +0000

    creating 256x256 video

--- lid_driven_gif.py
+++ lid_driven_gif.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import imageio
 
-# Directory containing frame HDF5 files
 frames_dir = 'frames_gif_ex'
 
 # Collect sorted file paths
@@ -24,6 +23,7 @@ for file_path in file_paths:
         X1 = f["X1"][:]
         X2 = f["X2"][:]
         a = f["a"][:]
+        b = f["b"][:]
         # Coordinates (assumes X, Y defined globally or regenerate here)
         Ny, Nx = phi.shape
         x = np.linspace(0, 1, Nx)  # adjust if domain differs
@@ -32,29 +32,32 @@ for file_path in file_paths:
 
     # Mask solid region
     a_masked = np.ma.masked_where(phi <= 0, a)
-    
+    b_masked = np.ma.masked_where(phi <= 0, b)
+    u_mag_masked = np.sqrt(a**2 + b**2)
+
     # Plot
-    fig, ax = plt.subplots(figsize=(6, 6))
-    cf = ax.contourf(X, Y, a_masked, levels=70, cmap="turbo")
+    fig, ax = plt.subplots(figsize=(4, 4))
+    cf = ax.contourf(X, Y, a_masked, levels=50, cmap="Spectral_r")
     ax.contourf(X, Y, phi <= 0, levels=[0.5, 1], colors='white', zorder=2)
     X1_masked = np.where(phi <= 0, X1, np.nan)
     X2_masked = np.where(phi <= 0, X2, np.nan)
-    ax.contour(X, Y, phi, levels=[0], colors='black', linewidths=3.5, zorder=3)
-    ax.contour(X, Y, X1_masked, levels=15, colors='black', linewidths=0.75, linestyles='solid', zorder=4)
-    ax.contour(X, Y, X2_masked, levels=15, colors='black', linewidths=0.75, linestyles='dashed', zorder=4)
-    ax.set_title("Deformed Mesh via Reference Map Contours")
+    ax.contour(X, Y, phi, levels=[0], colors='black', linewidths=1.5, zorder=3)
+    ax.contour(X, Y, X1_masked, levels=15, colors='black', linewidths=0.5, linestyles='solid', zorder=4)
+    ax.contour(X, Y, X2_masked, levels=15, colors='black', linewidths=0.5, linestyles='dashed', zorder=4)
+    ax.set_title("Deformation of Soft Disc in Lid-Driven Cavity Flow", fontsize=12)
     ax.set_aspect('equal')
     plt.axis('off')
     
     # Convert plot to image array
     fig.canvas.draw()
-    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
-    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    image = np.asarray(fig.canvas.buffer_rgba())
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (4,))
+
     images.append(image)
     plt.close(fig)
 
 # Save as GIF
 current_dir = os.getcwd()
-gif_path = os.path.join(current_dir, 'lid_driven.gif')
-imageio.mimsave(gif_path, images, fps=20)
+gif_path = os.path.join(current_dir, 'lid_driven_256x256.gif')
+imageio.mimsave(gif_path, images, fps=40)
 
